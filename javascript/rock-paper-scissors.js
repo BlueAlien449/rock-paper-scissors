cba8678919729547124436018bc14da8649fcd8d
BlueAlien449 <99145495+BlueAlien449@users.noreply.github.com>
2023-03-17 16:17:07 -0600

    Create game function add a for loop and call the play round function

--- javascript/rock-paper-scissors.js
+++ javascript/rock-paper-scissors.js
@@ -26,19 +26,26 @@ function playRound(playerSelection, computerSelection){
     let pChoice = frmtText1 + frmtText2;
     
     if (pChoice === "Rock" && computerSelection === "Scissors"){
-        winner = "You win, rock wins to scissors";
+        winner = "You win, rock beats scissors";
     } else if (pChoice === "Paper" && computerSelection === "Rock"){
-        winner = "You win, paper wins to rock";
+        winner = "You win, paper beats to rock";
     } else if (pChoice === "Scissors" && computerSelection === "Paper"){
-        winner = "You lose, scissors wins to paper";
+        winner = "You lose, scissors beats paper";
     } else if (pChoice === computerSelection){
         winner = "Tie, no one wins"
     } else {
         winner = "You lose"
     }
+
     console.log("You chose: " + pChoice + " computer chose: " + computerSelection);
     console.log(winner);
 
 }
 
-playRound();
+function game(){
+    for (let i = 0; i < 5; i++){
+        playRound();
+    }
+}
+
+game();
