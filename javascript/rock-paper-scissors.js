509e4298d29e991cb67ca5dac49935caf195cdac
BlueAlien449 <99145495+BlueAlien449@users.noreply.github.com>
2023-03-17 16:11:54 -0600

    Add If loop to play round function

--- javascript/rock-paper-scissors.js
+++ javascript/rock-paper-scissors.js
@@ -1,3 +1,5 @@
+let winner;
+
 function getComputerChoice(){
     let randInt = Math.floor(Math.random() * 3) +1;
     let oppsChoice;
@@ -22,8 +24,21 @@ function playRound(playerSelection, computerSelection){
     let frmtText1 = playerSelection.charAt(0).toUpperCase();
     let frmtText2 = playerSelection.substr(1).toLowerCase();
     let pChoice = frmtText1 + frmtText2;
-
+    
+    if (pChoice === "Rock" && computerSelection === "Scissors"){
+        winner = "You win, rock wins to scissors";
+    } else if (pChoice === "Paper" && computerSelection === "Rock"){
+        winner = "You win, paper wins to rock";
+    } else if (pChoice === "Scissors" && computerSelection === "Paper"){
+        winner = "You lose, scissors wins to paper";
+    } else if (pChoice === computerSelection){
+        winner = "Tie, no one wins"
+    } else {
+        winner = "You lose"
+    }
     console.log("You chose: " + pChoice + " computer chose: " + computerSelection);
+    console.log(winner);
+
 }
 
 playRound();
